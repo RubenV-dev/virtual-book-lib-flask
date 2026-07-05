5d5572ce2aaaf5f111ddca73516a71449ad3bce8
Ruben Vallejo <r.will16@hotmail.com>
2026-07-05 17:41:32 -0400

    add cors lib support and integrate into app.py

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ from config import Config
 from models.book import db
 from routes.book_routes import book_bp
 from errors.handlers import register_error_handlers
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
+
+CORS(
+    app,
+    resources={r"/api/*": {"origins": app.config["CORS_ORIGINS"]}}
+)
 
 db.init_app(app)
 
--- config.py
+++ config.py
@@ -5,3 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'database', 'books.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CORS_ORIGINS = [
+        "http://localhost:3000",
+        "http://localhost:5173",
+    ]
